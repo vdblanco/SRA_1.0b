4c834f5d3d3f4a07f45864f751e4b14d2f1a5814
vdblanco <107686581+vdblanco@users.noreply.github.com>
2022-06-19 22:17:10 +0200

    Update set_attrib.sh

--- set_attrib.sh
+++ set_attrib.sh
@@ -2,4 +2,4 @@
 # Mantain "i" attribute in NFS & SMB repositories during files lifetime (parameter)
 
 /usr/bin/find /backup/repository -type f -mtime +$1 -execdir chattr -i -- '{}' \;
-/usr/bin/find /backup/repository -type f \( ! -name *.vbm \) -mtime -$1 -amin +60 -execdir chattr +i -- '{}' \;
+/usr/bin/find /backup/repository -type f \( -name "*.vbk" -o -name "*.vib" -o -name "*.bkf" -o -name "*.bak*" -o -name "*.arc" \) -mtime -$1 -amin +60 -execdir chattr +i -- '{}' \;
